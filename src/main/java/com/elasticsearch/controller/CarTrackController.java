f87af6b0629f94522832f841b88116bfcd8ec6a4
xuewenbo <1711601459@qq.com>
2018-04-19 18:01:29 +0800

    完善Es查询模板

--- src/main/java/com/elasticsearch/controller/CarTrackController.java
+++ src/main/java/com/elasticsearch/controller/CarTrackController.java
@@ -2,13 +2,17 @@ package com.elasticsearch.controller;
 
 import com.elasticsearch.model.CarTrack;
 import com.elasticsearch.service.CarTrackService;
+import com.elasticsearch.util.ESClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+
 
 @Controller
 @RequestMapping(value = "/carTrack")   //汽车行驶轨迹
@@ -16,6 +20,128 @@ public class CarTrackController {
 
     @Autowired
     private CarTrackService carTrackService;
+    @Autowired
+    private ESClientUtil esClientUtil;
+
+
+    @RequestMapping("/saveDoc/{id}")
+    @ResponseBody
+    public String saveDoc(@PathVariable("id") String id){
+        HashMap<String, Object> m = new HashMap<>();
+        m.put("name","豆丁");
+        m.put("age","6");
+        m.put("location","39.955311,117.33200");
+        m.put("signalTime",System.currentTimeMillis());
+        String index = esClientUtil.saveDoc("dog", "doginfo", id, m);
+        return "创建成功："+index;
+    }
+
+    @RequestMapping("/updateDoc/{id}")
+    @ResponseBody
+    public String updateDoc(@PathVariable("id") String id){
+        HashMap<String, Object> m = new HashMap<>();
+        m.put("plateNo","京A55555");
+        m.put("SignalTransTime",System.currentTimeMillis());
+        m.put("location","39.955566,117.33311");
+        String docId = esClientUtil.updateDoc("car","car_track",id,m);
+        return "更新成功："+docId;
+    }
+
+
+    @RequestMapping("/deleteDoc/{id}")
+    @ResponseBody
+    public String deleteDoc(@PathVariable("id") String id){
+        String docId = esClientUtil.deleteById("car","car_track",id);
+        return "删除成功："+docId;
+    }
+
+
+    @RequestMapping("/getDocById/{id}")
+    @ResponseBody
+    public String getDocById(@PathVariable("id") String id){
+        String doc = esClientUtil.getDocById("car","car_track",id);
+        return "获取成功："+doc;
+    }
+
+    //创建索引，并添加映射mapping
+    @RequestMapping("/creatIndexAndMapping")
+    @ResponseBody
+    public String creatIndexAndMapping(){
+        String mapping = "{\n" +
+                "    \"doginfo\": {\n" +
+                "      \"properties\": {\n" +
+                "        \"name\": {\n" +
+                "          \"type\": \"string\"\n" +
+                "        }\n" +
+                "        \"location\": {\n" +
+                "          \"type\": \"geo_point\"\n" +
+                "        \"signalTime\": {\n" +
+                "          \"type\": \"long\"\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }";
+        boolean b = esClientUtil.creatIndexAndMapping("dog", "doginfo", mapping);
+        if(b==true){
+            return "映射添加成功！";
+        }
+        return "失败";
+    }
+
+
+    @RequestMapping("/getDocAll")
+    @ResponseBody
+    public String getDocAll(){
+        String docAll = esClientUtil.getDocAll("car", "car_track");
+        return docAll;
+    }
+
+    //字段值匹配查询
+    @RequestMapping("/multiSearch")
+    @ResponseBody
+    public String multiSearch(){
+        String multiSearch = esClientUtil.multiSearch("京A2222", "id", "2");
+        return multiSearch;
+    }
+
+
+    //字段值匹配查询
+    @RequestMapping("/fullTextQuery")
+    @ResponseBody
+    public String fullTextQuery(){
+        String result = esClientUtil.fullTextQuery("car","car_track","plateNo","京A2222");
+        return result;
+    }
+
+
+
+    //获取指定中心点一定范围内的文档
+    @RequestMapping("/getDocInDistance/{distance}")
+    @ResponseBody
+    public String getDocInDistance(@PathVariable("distance") String distance){
+        double lat = 39.929986;
+        double lon = 116.395645;
+        String docInDistance = esClientUtil.getDocMile("car", "car_track","location", lat, lon, Double.valueOf(distance));
+        return docInDistance;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @RequestMapping("/saveCarTrack")
     @ResponseBody
@@ -25,7 +151,6 @@ public class CarTrackController {
         carTrack.setPlateNo("豫B1111");
         carTrack.setSignalTransTime(System.currentTimeMillis());
         carTrack.setLocation("39.955551,117.33333");
-        carTrackService.saveCarTrackData(carTrack);
         return "存储成功！";
     }
 
--- src/main/java/com/elasticsearch/util/ESClientUtil.java
+++ src/main/java/com/elasticsearch/util/ESClientUtil.java
@@ -1,57 +1,72 @@
 package com.elasticsearch.util;
 
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.MultiSearchResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.geo.GeoDistance;
 import org.elasticsearch.common.unit.DistanceUnit;
 import org.elasticsearch.index.query.GeoDistanceQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static org.elasticsearch.index.query.QueryBuilders.geoDistanceQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
+@Service
 public class ESClientUtil {
 
     @Autowired
     private TransportClient client;
 
     /**
-     * 创建索引
+     * 添加纪录: 索引不存在时候，直接创建
      * @param index 索引名称
      * @param type 索引类型
      * @param id 索引id
      * @param json 是key:value数据类型，可以代表json结构.
      * @return
      */
-    public String createIndex(String index, String type,  Map<String,Object> json){
-        IndexResponse response = client.prepareIndex(index, type).setSource(json).get();
+    public String saveDoc(String index, String type,String id, Map<String,Object> json){
+        IndexResponse response = client.prepareIndex(index, type,id).setSource(json).get();
         return response.getIndex();
     }
 
 
-    /**
-     * 更新文档
-     *
-     * @param index
-     * @param type
-     * @param id
-     * @param doc
-     * @return
-     */
-    public String updateDoc(String index, String type, String id, Object doc) {
+
+
+        /**
+         * 更新文档
+         * @param index
+         * @param type
+         * @param id
+         * @param doc
+         * @return
+         */
+    public String updateDoc(String index, String type, String id, Map<String,Object> doc) {
         UpdateResponse updateResponse = client.prepareUpdate(index, type, id).setDoc(doc).get();
         return updateResponse.getId();
     }
 
     /**
-     * 删除索引
+     * 删除指定id文档
      * @param index
      * @param type
      * @param id
@@ -69,7 +84,7 @@ public class ESClientUtil {
      * @param id
      * @return
      */
-    public String getIdx(String index, String type, String id) {
+    public String getDocById(String index, String type, String id) {
         GetResponse response = client.prepareGet(index, type, id).get();
         if (response.isExists()) {
             return response.getSourceAsString();
@@ -77,15 +92,62 @@ public class ESClientUtil {
         return null;
     }
 
+
+    //全文搜索查询；  name:  字段；   text: 对应值
+    public String fullTextQuery(String index, String type, String name,String text) {
+        QueryBuilder qb = matchQuery(name,text);
+        SearchResponse searchResponse = client.prepareSearch(index).setTypes(type).setQuery(qb).get();
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        String docStr = "";
+        for(SearchHit doc:hits){
+            docStr += doc.getSourceAsString();
+        }
+        return docStr;
+    }
+
+
+
     /**
-     * 判断某个索引下type是否存在
+     * 获取对应的存储内容
      * @param index
      * @param type
+     * @param id
      * @return
      */
-    public boolean isTypeExist(String index, String type) {
-        return client.admin().indices().prepareTypesExists(index).setTypes(type).execute().actionGet().isExists();
+    public String getDocAll(String index, String type) {
+        SearchResponse response = client.prepareSearch().get();
+        SearchHit[] hits = response.getHits().getHits();
+        String docStr = "";
+        for(SearchHit doc:hits){
+            docStr += doc.getSourceAsString();
+        }
+        return docStr;
     }
+
+    /**
+     *  多条件查询聚合
+     * @param index
+     * @param type
+     * @param id
+     * @return
+     */
+    public  String  multiSearch(String queryString,String name,String text){
+        SearchRequestBuilder srb1 = client.prepareSearch().setQuery(QueryBuilders.queryStringQuery(queryString)).setSize(1);
+        SearchRequestBuilder srb2 = client.prepareSearch().setQuery(QueryBuilders.matchQuery(name, text)).setSize(1);
+        MultiSearchResponse sr = client.prepareMultiSearch().add(srb1).add(srb2).get();
+        String docStr = "";
+        for (MultiSearchResponse.Item item : sr.getResponses()) {
+            SearchResponse response = item.getResponse();
+            SearchHit[] hits = response.getHits().getHits();
+            for(SearchHit doc:hits){
+                docStr += doc.getSourceAsString();
+            }
+        }
+        return docStr;
+    }
+
+
+
 
     /**
      * 判断索引是否存在
@@ -97,13 +159,84 @@ public class ESClientUtil {
     }
 
     /**
+     * 创建一个空索引
+     * @param indexName
+     * @return
+     */
+    public boolean creatIndex(String indexName){
+        CreateIndexResponse indexResponse = client.admin().indices().prepareCreate(indexName).execute().actionGet();
+        return indexResponse.isAcknowledged();
+    }
+
+    /**
+     * 创建索引并添加映射
+     * @param indexName
+     * @param type
+     * @param mapping
+     * @return     index 若已经存在则不能添加mapping
+     */
+    public boolean creatIndexAndMapping(String indexName,String type,String mapping){
+        CreateIndexResponse indexResponse = client.admin().indices().prepareCreate("dog").addMapping(mapping).get();
+        return indexResponse.isAcknowledged();
+    }
+
+
+    //mapping范例
+    public void getMapping(){
+        String mapping = "{\n" +
+                "    \"doginfo\": {\n" +
+                "      \"properties\": {\n" +
+                "        \"name\": {\n" +
+                "          \"type\": \"string\"\n" +
+//                "             \"indexAnalyzer\": \"ik\"\n"+
+                "        }\n" +
+
+                "        \"age\": {\n" +
+                "          \"type\": \"string\"\n" +
+//                "             \"indexAnalyzer\": \"ik\"\n"+
+                "        }\n" +
+
+                "      }\n" +
+                "    }\n" +
+                "  }";
+    }
+
+
+    /**
+     * 为index，添加mapping
+     * @param indexName
+     * @param indexType
+     * @param mapping
+     * @return
+     */
+    public  boolean createMapping(String indexName,String indexType,String mapping) throws IOException {
+        PutMappingRequest putMapping = Requests.putMappingRequest(indexName).type(indexType).source(mapping);
+        PutMappingResponse pr = client.admin().indices().putMapping(putMapping).actionGet();
+        return pr.isAcknowledged();
+    }
+
+
+
+
+    /**
+     * 判断某个索引下type是否存在
+     * @param index
+     * @param type
+     * @return
+     */
+    public boolean isTypeExist(String index, String type) {
+        return client.admin().indices().prepareTypesExists(index).setTypes(type).execute().actionGet().isExists();
+    }
+
+
+    /**
      * 创建type（存在则进行更新）
      * @param index 索引名称
      * @param type type名称
      * @param o 要设置type的object
      * @return
      */
-    public boolean createType(String index, String type, Object o) {
+    public boolean createType(String index, String type, Map<String,Object> o) {
         if (!isIndexExist(index)) {
             System.out.println("索引不存在");
             return false;
@@ -118,13 +251,50 @@ public class ESClientUtil {
         }
     }
 
+    /**
+     * 获取中心点周围指定范围内的文档
+     * @param index 索引名称
+     * @param locationProperty 地理位置属性名
+     * @param lat 纬度
+     * @param lon 经度
+     * @param distance 距离/Km
+     * @return
+     */
 
-    public List getObjWithin(String index,String locationProperty,double lat,double lon,double distance){
-        GeoDistanceQueryBuilder query = geoDistanceQuery(locationProperty);
-        QueryBuilder qb = query.point(lat, lon).distance(distance, DistanceUnit.KILOMETERS);
-        SearchResponse response = client.prepareSearch(index).setQuery(qb).get();
-        return null;
+    public String getDocInDistance(String index,String type,String locationProperty,double lat,double lon,double distance){
+
+        SearchRequestBuilder sr = client.prepareSearch(index).setTypes(type);
+//        sr.setFrom(0).setSize(1000);//设置获取的数量为0--1000
+        GeoDistanceQueryBuilder geoBuilder = geoDistanceQuery(locationProperty).point(lat, lon).distance(distance, DistanceUnit.METERS);
+        sr.setPostFilter(geoBuilder);
+        SearchResponse searchResponse = sr.execute().actionGet();
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        String docStr = "";
+        for (SearchHit doc:hits){
+            docStr += doc.getSourceAsString();
+        }
+        return docStr;
     }
+
+
+    //中心点周边文档查询
+    public String getDocMile(String index,String type,String locationProperty,double lat,double lon,double distance){
+        QueryBuilder qb = geoDistanceQuery(locationProperty)
+                .point(lat, lon)
+                .distance(distance, DistanceUnit.KILOMETERS)
+                .optimizeBbox("memory")
+                .geoDistance(GeoDistance.ARC);
+
+        SearchResponse searchResponse = client.prepareSearch(index).setTypes(type).setQuery(qb).get();
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        String docStr = "";
+        for (SearchHit doc:hits){
+            docStr += doc.getSourceAsString();
+        }
+        return docStr;
+    }
+
+
 
 
 }
